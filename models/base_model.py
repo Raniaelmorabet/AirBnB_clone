238fcf72308dcfcd0d14a2cdf06f76eab3b3a7de
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-07-15 16:32:38 +0100

    add: base clase task 3

---
+++ models/base_model.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+"""This module defines a base class for all models in our hbnb clone"""
+
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """A base class for all hbnb models"""
+    def __init__(self, *args, **kwargs):
+        """Instantiates a new model
+        Args:
+            *args: Unused
+            **kwargs: A dictionary of arguments and values for the
+            instantiation of the model
+        Attributes:
+            id: unique id generated
+            created_at: datetime object set at the time of creation
+            updated_at: datetime object set at the time of creation and
+            updated whenever the object is changed
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value,
+                            "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == "__class__":
+                    continue
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            # models.storage.new(self)
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """Updates updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        # models.storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__"""
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        return my_dict
