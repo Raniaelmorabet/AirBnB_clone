65fae616760fdd82c0cd5778fad83c435489beac
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-07-15 17:03:43 +0100

    add: more uuid unittests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,6 +7,7 @@ Unittest for BaseModel class
 import unittest
 import pep8
 import json
+import uuid
 from datetime import datetime
 from models.base_model import BaseModel
 
@@ -38,12 +39,45 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(isinstance(self.bm1, BaseModel))
         self.assertTrue(isinstance(self.bm2, BaseModel))
 
+    # --- Testing UUID --- #
     def test_base_model_id(self):
         """Test id attribute in BaseModel class"""
         self.assertTrue(hasattr(self.bm1, "id"))
         self.assertTrue(hasattr(self.bm2, "id"))
         self.assertNotEqual(self.bm1.id, self.bm2.id)
 
+    def test_base_model_id_format(self):
+        """Test id format in BaseModel class"""
+        self.assertIsInstance(uuid.UUID(self.bm1.id), uuid.UUID)
+
+    def test_base_model_id_unique(self):
+        """Test id uniqueness in BaseModel class"""
+        self.assertNotEqual(self.bm1.id, self.bm2.id)
+
+    def test_base_model_id_type(self):
+        """Test id type in BaseModel class"""
+        self.assertEqual(type(self.bm1.id), str)
+        self.assertEqual(type(self.bm2.id), str)
+
+    def test_base_model_id_length(self):
+        """Test id length in BaseModel class"""
+        self.assertEqual(len(self.bm1.id), 36)
+        self.assertEqual(len(self.bm2.id), 36)
+
+    def test_base_model_uuid_error(self):
+        """
+        Tests a badly named UUID, to confirm that it is checked.
+        """
+        bm3 = BaseModel()
+        bm3.id = 'Wrong ID'
+        warn = 'badly formed hexadecimal UUID string'
+
+        with self.assertRaises(ValueError) as msg:
+            uuid.UUID(bm3.id)
+
+        self.assertEqual(warn, str(msg.exception))
+
+    # --- Testing datetime --- #
     def test_base_model_created_at(self):
         """Test created_at attribute in BaseModel class"""
         self.assertTrue(hasattr(self.bm1, "created_at"))
