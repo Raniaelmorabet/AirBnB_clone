0fcfe866db4941e701551e72b5474dbfe3625632
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-07-16 19:23:41 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -42,7 +42,8 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-            storage.save()
+
+storage.save()
 
     def __str__(self):
         """
