3073cc56c7e129616a2bf20940eda6e8b8881fe9
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-07-16 18:14:21 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 import uuid
 from datetime import datetime
 import models
+from models import storage
 
 
 class BaseModel:
@@ -37,6 +38,9 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
+
+    storage.save()
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -45,6 +49,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__"""
