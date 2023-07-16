5e85fc2b418275994278e95ef2b9621eca20be86
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-07-16 15:29:40 +0200

    Update console.py

--- console.py
+++ console.py
@@ -2,27 +2,21 @@
 
 import cmd
 
-
 class HBNBCommand(cmd.Cmd):
-    """
-    HBNBCommand class - entry point of the command interpreter
-    """
     prompt = '(hbnb) '
-    allowed_classes = ['BaseModel', 'User', 'State', 'City',
-                       'Amenity', 'Place', 'Review']
-
-    def emptyline(self):
-        """Do nothing when empty line is entered"""
-        pass
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """EOF command to exit the program"""
+        """Exit the program with EOF (Ctrl+D)"""
+        print()
         return True
 
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
