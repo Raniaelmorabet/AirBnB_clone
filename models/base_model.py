6e4b2d75664d098bf361ef47e1b4e809b8dfa0ea
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-07-16 17:06:38 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,64 +1,80 @@
 #!/usr/bin/python3
+"""Defines the BaseModel class.
 
-"""This module defines a base class for all models in our hbnb clone"""
+This module provides the BaseModel class, which serves as the base class for
+other classes in the project. It defines common instance attributes and methods.
+
+Attributes:
+    id (str): A UUID assigned to each instance upon creation.
+    created_at (datetime): The datetime when an instance is created.
+    updated_at (datetime): The datetime when an instance is updated.
+
+Methods:
+    __init__(self, *args, **kwargs): Initializes a new instance of the class.
+    __str__(self): Returns a string representation of the instance.
+    save(self): Updates the instance's updated_at attribute and saves the changes.
+    to_dict(self): Returns a dictionary representation of the instance.
+
+"""
 
 import uuid
+import models
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """The BaseModel class serves as the base class for other classes.
+
+    It defines common instance attributes and methods.
+
+    """
 
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model
+        """Initializes a new instance of the class.
 
         Args:
-            *args: Unused
-            **kwargs: A dictionary of arguments and values for the
-                instantiation of the model
+            *args: Unused positional arguments.
+            **kwargs: A dictionary of arguments and values for instantiation.
 
-        Attributes:
-            id: Unique ID generated
-            created_at: Datetime object set at the time of creation
-            updated_at: Datetime object set at the time of creation and
-                updated whenever the object is changed
         """
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(
-                        self,
-                        key,
-                        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    )
-                elif key == "__class__":
-                    continue
-                else:
+                if key == "created_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
                     setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.id = str(uuid.uuid4())  # Creating a unique id
+            self.created_at = datetime.now()  # Creating current time when an instance is created
+            self.updated_at = datetime.now()  # Creating current time when an instance is updated
+            models.storage.new(self)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__,
-            self.id,
-            self.__dict__
-        )
+        """Returns a string representation of the instance.
+
+        Returns:
+            str: A string representation of the instance.
+
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with the current datetime"""
+        """Updates the instance's updated_at attribute and saves the changes."""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__"""
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = self.__class__.__name__
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        return my_dict
+        """Returns a dictionary representation of the instance.
+
+        Returns:
+            dict: A dictionary containing all keys and values of the instance.
+
+        """
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
