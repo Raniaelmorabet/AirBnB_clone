2d0355ae828c9327387f272f8849f25aac5f3d14
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-07-16 23:37:21 +0100

    add task 11

--- console.py
+++ console.py
@@ -148,13 +148,12 @@ class HBNBCommand(cmd.Cmd):
             print(count)
 
     def precmd(self, line):
-        """Defines instructions to execute before <line> is interpreted.
-        """
         if not line:
             return '\n'
 
         pattern = re.compile(r"(\w+)\.(\w+)\((.*)\)")
         match_list = pattern.findall(line)
+
         if not match_list:
             return super().precmd(line)
 
@@ -162,28 +161,18 @@ class HBNBCommand(cmd.Cmd):
         if not match_tuple[2]:
             if match_tuple[1] == "count":
                 instance_objs = storage.all()
-                print(len([
-                    v for _, v in instance_objs.items()
-                    if type(v).__name__ == match_tuple[0]]))
-                return "\n"
-            return "{} {}".format(match_tuple[1], match_tuple[0])
-        else:
-            args = match_tuple[2].split(", ")
-            if len(args) == 1:
-                return "{} {} {}".format(
-                    match_tuple[1], match_tuple[0],
-                    re.sub("[\"\']", "", match_tuple[2]))
-            else:
-                match_json = re.findall(r"{.*}", match_tuple[2])
-                if match_json:
-                    return "{} {} {} {}".format(
-                        match_tuple[1], match_tuple[0],
-                        re.sub("[\"\']", "", args[0]),
-                        re.sub("\'", "\"", match_json[0]))
-                return "{} {} {} {} {}".format(
-                    match_tuple[1], match_tuple[0],
-                    re.sub("[\"\']", "", args[0]),
-                    re.sub("[\"\']", "", args[1]), args[2])
+                count = sum(1 for v in instance_objs.values() if
+                            isinstance(v, globals()[match_tuple[0]]))
+                print(count)
+            return "\n"
+
+        args = match_tuple[2].split(", ")
+        args = [re.sub("[\"\']", "", arg) for arg in args]
+        if len(args) > 1:
+            match_json = re.findall(r"{.*}", match_tuple[2])
+            if match_json:
+                args[1] = re.sub("\'", "\"", match_json[0])
+        return "{} {} {}".format(match_tuple[1], match_tuple[0], ' '.join(args))
 
 
 if __name__ == "__main__":
