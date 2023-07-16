b3ec621653e819d80cd303dc0d63fd7f0ada7c0c
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-07-16 23:10:55 +0100

    fix: pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" This is a file storage that serializes an instance to a JSON file (JSON.dump)
+"""
+    This is a file storage that serializes an
+    instance to a JSON file (JSON.dump)
     It also deserializes a JSON file to an instance (JSON.load)
 """
 
@@ -14,32 +16,33 @@ from models.user import User
 
 
 class FileStorage:
-    """This class serializes instance to a JSON file and deserializes JSON to instance"""
-    __file_path = "file.json" #string - path to the JSON file (ex: file.json)
-    __objects = {} #empty dict, but will store all objects by <class name>.id
+    """This class serializes instance to a JSON file and
+    deserializes JSON to instance"""
+    __file_path = "file.json"  # string - path to the JSON file (ex: file.json)
+    __objects = {}  # empty dict, but will store all objects by <class name>.id
 
     def all(self):
         """returns the dictionary __objects"""
         return self.__objects
-    
+
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id
             all it does is to get the key of the form <obj class name>.id
         """
         key = obj.__class__.__name__ + "." + str(obj.id)
         self.__objects[key] = obj
-    
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        json_obj = {} #creates an empty dictionary
+        json_obj = {}  # creates an empty dictionary
 
         """Next, fill dictionary with an __objects element"""
         for key in self.__objects:
             json_obj[key] = self.__objects[key].to_dict()
-        
+
         with open(self.__file_path, 'w') as file:
             json.dump(json_obj, file)
-    
+
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
