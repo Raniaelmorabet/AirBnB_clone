35f27434fa76e28770f60c4bd5f42caa4b6799c4
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-07-16 19:32:54 +0200

    Update console.py

--- console.py
+++ console.py
@@ -1,28 +1,159 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Console module for interacting with the AirBnB clone models
+"""
 
 import cmd
+import sys
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """
-    HBNBCommand class - entry point of the command interpreter
+    Console class for the command interpreter
     """
-    prompt = '(hbnb) '
-    allowed_classes = ['BaseModel', 'User', 'State', 'City',
-                       'Amenity', 'Place', 'Review']
+    prompt = "(hbnb) "
 
     def emptyline(self):
-        """Do nothing when empty line is entered"""
+        """
+        Handles empty line input
+        """
         pass
 
+    def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel, saves it, and prints the id
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance based on the class name and id
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        if args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instances = storage.all()
+        instance_key = args[0] + "." + args[1]
+        if instance_key in instances:
+            print(instances[instance_key])
+        else:
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        if args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instances = storage.all()
+        instance_key = args[0] + "." + args[1]
+        if instance_key in instances:
+            del instances[instance_key]
+            storage.save()
+        else:
+            print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based or not on the class name
+        """
+        if not arg:
+            print([str(value) for value in storage.all().values()])
+        else:
+            args = arg.split()
+            if args[0] not in ["BaseModel"]:
+                print("** class doesn't exist **")
+                return
+
+            print([str(value) for key, value in storage.all().items()
+                   if args[0] in key])
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or updating attribute
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        if args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instances = storage.all()
+        instance_key = args[0] + "." + args[1]
+        if instance_key not in instances:
+            print("** no instance found **")
+            return
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        instance = instances[instance_key]
+        setattr(instance, args[2], type(getattr(instance, args[2]))(args[3]))
+        instance.save()
+
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """
+        Quit command to exit the program
+        """
         return True
 
     def do_EOF(self, arg):
-        """EOF command to exit the program"""
+        """
+        Handles the end of file (EOF) signal to exit the program
+        """
+        print()
         return True
 
+    def postloop(self):
+        """
+        Prints a new line after executing a command
+        """
+        print()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
